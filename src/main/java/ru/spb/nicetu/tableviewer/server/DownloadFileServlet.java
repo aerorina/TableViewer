dacf12ad5135fe1699b7eda6ae4274a8a8b6f6df
aerorina <arina.rudakova@inbox.ru>
2015-09-27 18:23:13 +0300

    Копирование файла с сервера на клиент

---
+++ src/main/java/ru/spb/nicetu/tableviewer/server/DownloadFileServlet.java
@@ -0,0 +1,56 @@
+package ru.spb.nicetu.tableviewer.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Передача файла с сервера на клиент, которая вызывается из браузера и предлагает скачиваемый файл сохранить.
+ *
+ * @author arudakova
+ */
+@WebServlet("/DownloadFileServlet")
+public class DownloadFileServlet  extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        String filePath = getServletContext().getRealPath("") + "/output.html";
+        File downloadFile = new File(filePath);
+        FileInputStream inStream = new FileInputStream(downloadFile);
+
+        ServletContext context = getServletContext();
+
+        String mimeType = context.getMimeType(filePath);
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+        response.setHeader(headerKey, headerValue);
+
+        OutputStream outStream = response.getOutputStream();
+
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+
+        inStream.close();
+        outStream.close();
+    }
+}
